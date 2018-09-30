512e8461d3897538b82030454ad764b0fd048143
Tim Allen <tim@noblesamurai.com>
2018-10-01 09:38:13 +1000

    Additional test.

--- test/index.js
+++ test/index.js
@@ -1,4 +1,6 @@
 const expect = require('chai').expect;
+require('chai').use(require('sinon-chai'));
+const sinon = require('sinon');
 const { Writable } = require('stream');
 
 describe('amqp-write-stream', function () {
@@ -6,5 +8,14 @@ describe('amqp-write-stream', function () {
     const channel = { publish: async () => {} };
     const ws = require('..')(channel, 'exchange', 'routingKey');
     expect(ws).to.be.an.instanceof(Writable);
+  });
+
+  it('should publish the payloads to AMQP', function () {
+    const publish = sinon.stub().resolves();
+    const channel = { publish };
+    const ws = require('..')(channel, 'exchange', 'routingKey', { stream: { objectMode: true } });
+    const payload = { hi: 'there' };
+    ws.write(payload);
+    expect(publish).to.have.been.calledWith('exchange', 'routingKey', payload);
   });
 });
